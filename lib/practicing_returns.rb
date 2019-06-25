bf46d972093e32e2060342c8ec77d48c70122bf1
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-25 13:25:33 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def hello(array)
   i = 0
   while i < array.length
@@ -5,5 +7,7 @@ def hello(array)
     i += 1
   end
 end
+
+binding.pry 
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
